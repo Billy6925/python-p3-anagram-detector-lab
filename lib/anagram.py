fd13c13a0024bbcc6a38cf7ae244d34920f64c84
Billy Eskein <billyeskein@gmail.com>
2025-02-19 18:10:55 +0300

    Update lib/anagram.py

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
 # your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self.word =word
+    def match(self,word):
+        sorted_word = sorted(self.word)
+        return ([w for w in word if sorted(w) == sorted(self.word)])
